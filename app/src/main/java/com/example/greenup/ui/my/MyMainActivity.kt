43b7a1abf8efda65f86dfb494550fdee494f501a
ryujisung <jiyesung1227@naver.com>
2023-07-14 23:29:46 +0900

    commit

--- app/src/main/java/com/example/greenup/ui/my/MyMainActivity.kt
+++ app/src/main/java/com/example/greenup/ui/my/MyMainActivity.kt
@@ -1,15 +1,19 @@
 package com.example.greenup.ui.my
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.example.greenup.R
 import com.example.greenup.databinding.ActivityFoodScanBinding
 import com.example.greenup.databinding.ActivityMainBinding
 import com.example.greenup.databinding.ActivityMyMainBinding
+import com.example.greenup.ui.LoginActivity
 import com.example.portplay.base.BaseActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -41,12 +45,29 @@ class MyMainActivity: BaseActivity<ActivityMyMainBinding>(R.layout.activity_my_m
             }
         binding.version = "현재 버전 0.0.1"
 
+        binding.myTxtLogout.setOnClickListener {
+            var intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
 
-
-        findViewById<TextView>(R.id.my_txt_noti).setOnClickListener {
+        binding.myTxtNoti.setOnClickListener {
+            var intent = Intent(this, NotiActivity::class.java)
+            startActivity(intent)
+        }
+        binding.myTxtEdit.setOnClickListener {
+            var intent = Intent(this, NotiActivity::class.java)
+            startActivity(intent)
+        }
+        binding.myTxtPw.setOnClickListener {
+            var intent = Intent(this, NotiActivity::class.java)
+            startActivity(intent)
+        }
+        binding.myTxtNoti2.setOnClickListener {
             var intent = Intent(this, NotiActivity::class.java)
             startActivity(intent)
         }
 
+
     }
+
 }
